9b41bfa792d1932b5103140ba0dd1e85eb984652
venedikttsulenev <venedikttsulenev@list.ru>
2021-01-30 23:00:49 +0300

    v1.2

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef WLOG_VERSION_H
 #define WLOG_VERSION_H
 
-#define VERSION "1.1.1"
-#define RELEASE_DATE "27 Jan 2021"
+#define VERSION "1.2"
+#define RELEASE_DATE "30 Jan 2021"
 
 #endif //WLOG_VERSION_H
